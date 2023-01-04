18b5de2f35178975bfbc238dab910aa204a7b41a
kubq01 <kuba18062001@gmail.com>
2023-01-04 13:14:50 +0100

    finished linking get requests for Admin; need to work on post request and passing parameters on get request

--- src/main/java/com/example/QuizApp/data/users/UserController.java
+++ src/main/java/com/example/QuizApp/data/users/UserController.java
@@ -1,6 +1,104 @@
-/*
+
 package com.example.QuizApp.data.users;
 
+import com.example.QuizApp.data.Class.Class;
+import com.example.QuizApp.data.Class.ClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+    private ClassService classService;
+
+    @Autowired
+    public UserController(UserService userService, ClassService classService)
+    {
+        this.userService = userService;
+        this.classService = classService;
+    }
+
+    @GetMapping("/aIndex")
+    public String showAdminIndex( Model model)
+    {
+        return "admin/adminIndex";
+    }
+
+    @GetMapping("/myAccount")
+    public String showAccount(Model model)
+    {
+        return "misc/userSelf";
+    }
+
+    @GetMapping("/myAccount/changePassword")
+    public String changePassword(Model model)
+    {
+        //TODO:add post mapping for changing password
+        return "misc/changePasswordSelf";
+    }
+
+    @GetMapping("/aIndex/userList")
+    public String listUser(Model model)
+    {
+        return "admin/listUsers";
+    }
+
+    @GetMapping("/aIndex/userList/userInfo")
+    public String userInfo(Model model)
+    {
+        return "admin/userInfo";
+    }
+
+    @GetMapping("aIndex/adminClasses")
+    public String showClassesForAdmin(Model model)
+    {
+        return "admin/listClassesForAdmin";
+    }
+
+    @GetMapping("aIndex/adminClasses/addClass")
+    public String addClass(Model model)
+    {
+        return "admin/addClass";
+    }
+
+    @PostMapping("aIndex/adminClasses/addClass/new")
+    public String insertClass(@ModelAttribute Class newClass)
+    {
+        classService.insert(newClass);
+        return "redirect:/user/aIndex/adminClasses";
+    }
+
+    @GetMapping("/aIndex/addUserMenu")
+    public String addUserMenu(Model model)
+    {
+        return "admin/addNewUser";
+    }
+
+    @GetMapping("/aIndex/addUserMenu/addStudent")
+    public String addUser(Model model)
+    {
+        return "admin/addStudent";
+    }
+
+    @GetMapping("/aIndex/addUserMenu/addTeacher")
+    public String addTeacher(Model model)
+    {
+        return "admin/addTeacher";
+    }
+
+    @GetMapping("/aIndex/addUserMenu/addAdmin")
+    public String addAdmin(Model model)
+    {
+        return "admin/addAdmin";
+    }
+
+}
+
+/*
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
